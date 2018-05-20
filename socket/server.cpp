e67d327827cc7c714ee6cb0d893b73fe03512717
mengqinggang <mqg@email.com>
2018-05-20 23:53:50 +0800

    socket

--- socket/server.cpp
+++ socket/server.cpp
@@ -19,8 +19,6 @@ using namespace std;
 int main() {
 
     struct sockaddr_in serveraddr, clientaddr;
-//    int sockfd, confd, i, len;
-//	socklen_t addrlen;
     char ipstr[128];
     char buf[MAXLINE];
 
@@ -39,15 +37,15 @@ int main() {
     // 3.listen
     listen(sockfd, 128);
 
-    while (1) {
+    while(1) {
         // 4.accept阻塞监听客户端链接请求
         socklen_t addrlen = sizeof(clientaddr);
         int confd = accept(sockfd, (struct sockaddr*)&clientaddr, &addrlen);//返回的是客户端和服务端专用通道的socket描述符
+
         // 输出客户端IP地址和端口号
         string client_ip = inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr, ipstr, sizeof(ipstr));
 		int client_port = ntohs(clientaddr.sin_port);
 		cout << "Client IP " << client_ip << " Port " << client_port << endl;
-//        printf("client ip %s\tport %d\n", inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr, ipstr, sizeof(ipstr)), intohs(clientaddr.sin_port));
 
         // 和客户端交互数据操作confd
         // 5.处理客户端请求 
